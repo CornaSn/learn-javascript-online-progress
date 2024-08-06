c9d20c646cf2b4a72ea2bbf33b0c49582597be8f
CornaSn <cornelia.schenk@hotmail.com>
2024-08-06 12:39:05 +0200

    Finish lesson 4 - variables

---
+++ lesson4.js
@@ -0,0 +1,7 @@
+// Define a variable called count with an original value of 0 and then increment it (add 1 to it) on the following line.
+
+let count = 0;
+count = +1;
+
+// Define a variable ageLimit that cannot be re-assigned and give it a value of 18.
+const ageLimit = 18;
